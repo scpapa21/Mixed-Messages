369b8da5fd22d6703beef3b84368ade283871176
scpapa21 <scalderonpapa@gmail.com>
2022-02-09 17:34:53 -0500

    Basic Game Result Message

--- Main.js
+++ Main.js
@@ -1,10 +1,20 @@
-const matchDays = ["Monday","Tuesday","Wednsday","Thursday","Friday","Saturday","Sunday"];
+const matchDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
 const teamsPlaying = ["Real Madrid","Barcelona","Atletico de Madrid","Villareal","Valencia",
 "Chelsea","Liverpool","Manchester United","Manchester City","Arsenal","Tottenham",
 "Juventus","AC Milan","Inter","Roma","Napoli","Atalanta", "PSG", "Lyon", "Monaco",
-"Bayern Munich", "Dortmund", "Bayern Leverkusen", "RB Leipzig", "FC Schalke 04"]
+"Bayern Munich", "Dortmund", "Bayern Leverkusen", "RB Leipzig", "FC Schalke 04"];
 
 const results = ["win 1-0","win 2-0","win 3-0","win 4-0","win 5-0","win 2-1","win 3-1","win 4-2","win 4-3","win 5-2","win 5-4",
 "draw 0-0","draw 1-1","draw 2-2","draw 3-3","draw 4-4",
-"lose 0-1","lose 0-2","lose 0-3","lose 0-4","lose 0-5","lose 0-6","lose 1-2","lose 1-3","lose 1-4","lose 2-3","lose 3-4","lose 2-5"]
+"lose 0-1","lose 0-2","lose 0-3","lose 0-4","lose 0-5","lose 0-6","lose 1-2","lose 1-3","lose 1-4","lose 2-3","lose 3-4","lose 2-5"];
+
+const randomNumberDays = Math.floor(Math.random()*matchDays.length);
+
+const randomNumberTeams = Math.floor(Math.random()*teamsPlaying.length);
+
+const randomNumberResult = Math.floor(Math.random()*results.length);
+
+const mixedMessage = `This ${matchDays[randomNumberDays]} ${teamsPlaying[randomNumberTeams]} is going to ${results[randomNumberResult]}!`;
+
+console.log(mixedMessage);
